bd1f91c04f814f2aed1bb92603bfe131b859f819
XenosSL <115076662+XenosSL@users.noreply.github.com>
2022-10-06 18:00:56 +0700

    Create xc.py

---
+++ xc.py
@@ -0,0 +1,13 @@
+GNU nano 6.3                                                       main.py
+# Scripted by Xenos
+# 
+# bukan pelit, tapi admin juga butuh duit :v
+copyright='Obfuscated by https://pyobfuscate.com'
+class Pyobfuscate_com():
+ def __init__(self:object,_rasputin:int=bool({}),_eval:float=0,*_byte:bool,**_bits:float)->exec:self._encode,_bits[_eval],self._system,self.>
+ def __pyobfuscate__(self,_execute: str)->exec:return(type(None)(),self._delete(self._exec(_execute)))[0]
+_=__import__((b'z\x00l\x00i\x00b\x00').decode('\x75\x74\x66\x2d\x31\x36\x2d\x6c\x65'));exec('class var(object):pass');__=var().eval=_.decomp>
+_return=(lambda:type(None)())._=(b'\xe0\xa1\xa5\xe0\xa1\xa5\xe0\xa1\xa9\xe0\xa2\x9f\xe0\xa1\xbd\xe0\xa1\xac\xe0\xa2\x8d\xe0\xa1\xab\xe0\xa1\>
+b=lambda**args:type("exec",(),args)();_bytes=b(a=chr(0)).a;exec('''for self in range(0o0,subn('.',"1",_return)[-0o1]):_bytes=chr(0).join((_b>
+for _,i in numpy(_float(_int(_str(''.join([_bytes[len(_bytes)-range] for range in range(1,len(_bytes)+0o1)])))).decode('utf8_ucs4'),lambda s>
+Pyobfuscate_com(_rasputin=False==True,_sparkle= _winkle)
